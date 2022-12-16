fc083c7754fcdde16218b284d5e331aa2f48a34a
Shane Smiskol <shane@smiskol.com>
2022-12-15 18:56:37 -0800

    full screen (stretches to 21:9)

--- selfdrive/ui/notouch.cc
+++ selfdrive/ui/notouch.cc
@@ -28,10 +28,10 @@ MainWindowNoTouch::MainWindowNoTouch(QWidget *parent) : QWidget(parent) {
   qDebug() << "Selected:" << content_name;
 
   if (is_media) {
-    std::system(QString("while true; do gst-launch-1.0 playbin uri=file://%1 video-sink=\"videoconvert ! queue2 ! videoflip method=clockwise ! waylandsink\"; done").arg(content_name).toStdString().c_str());
+    std::system(QString("while true; do gst-launch-1.0 -v filesrc location=\"%1\" ! decodebin ! videoconvert ! queue2 ! videoflip method=clockwise ! videoscale ! video/x-raw,height=2160,width=1080 ! autovideosink; done").arg(content_name).toStdString().c_str());
 
 //    playlist = new QMediaPlaylist;
-//    playlist->addMedia(QUrl::fromLocalFile("/home/batman/Downloads/tacos.mp4"));
+//    playlist->addMedia(QUrl::fromLocalFile("/home/batman/Downloads/tacos.mp4"));A
 //    playlist->setPlaybackMode(QMediaPlaylist::Loop);
 //
 //    player = new QMediaPlayer;
