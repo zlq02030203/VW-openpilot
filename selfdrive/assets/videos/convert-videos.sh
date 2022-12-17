d5559e790b3439e4fb1c23e26d4ef44297bc9f4f
Shane Smiskol <shane@smiskol.com>
2022-12-16 21:23:50 -0800

    convert to h264

--- selfdrive/assets/videos/convert-videos.sh
+++ selfdrive/assets/videos/convert-videos.sh
@@ -6,7 +6,7 @@ echo "converting videos to mpeg4"
 mkdir -p out/
 
 for f in *.mp4; do
-  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -c:v mpeg4 "out/$f"
+  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -c:v h264 -strict -2 "out/$f"
 done;
 
 echo "Success!"
