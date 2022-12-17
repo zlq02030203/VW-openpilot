dff2b0f08ef4f093d84764ab15bfe14e3b3117da
Shane Smiskol <shane@smiskol.com>
2022-12-16 20:24:45 -0800

    launch_openpilot updates

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
-set -e
-
 # wait for weston to come up
 sleep 5
 
@@ -9,21 +7,26 @@ echo "Building" | ./selfdrive/ui/spinner &
 spinner_pid=$!
 
 # build
-scons selfdrive/ui/_notouch selfdrive/ui/_ui -j8 --extras || true
+scons selfdrive/ui/_notouch selfdrive/ui/_ui -j8 --extras
 kill -9 $spinner_pid
 
 # launch ui and let user set up ssh
-./selfdrive/ui/ui || true
+./selfdrive/ui/ui
+
+# update
+git fetch
+git reset --hard "@{u}"
+git submodule update --init --recursive
 
 echo "Installing dependencies" | ./selfdrive/ui/spinner &
 spinner_pid=$!
 
 # install deps
 sudo apt update
-sudo apt-get install -y --no-install-recommends gstreamer1.0-tools gstreamer1.0-libav gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad || true
+sudo apt-get install -y --no-install-recommends gstreamer1.0-tools gstreamer1.0-libav gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad
 kill -9 $spinner_pid
 
-# convert videos to mpeg4
+# download videos
 echo "Downloading videos" | ./selfdrive/ui/spinner &
 spinner_pid=$!
 ./selfdrive/assets/videos/download-videos.sh
