a4170a398c4eac2fb4cffa5a2c08093199a2c14f
Shane Smiskol <shane@smiskol.com>
2022-12-19 16:50:01 -0800

    draft

--- selfdrive/assets/videos/convert-videos.sh
+++ selfdrive/assets/videos/convert-videos.sh
@@ -6,7 +6,10 @@ echo "converting videos to mpeg4"
 mkdir -p out/
 
 for f in *.mp4; do
-  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -c:v h264 -strict -2 "out/$f"
+#  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -c:v h264 -strict -2 "out/$f"
+#  ffmpeg -y -i "$f" -vf "scale=2160:1080,setpts=PTS-STARTPTS" -r 20 -c:v h264 -strict -2 "out/$f"
+  ffmpeg -y -i "$f" -vf "scale=2160:1080,setpts=PTS-STARTPTS" -r 20 -c:v h264 -strict -2 -movflags faststart "out/$f"
+
 done;
 
 echo "Success!"
