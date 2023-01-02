3bb23bdd82fc5c010b426614677b994c7997b553
Shane Smiskol <shane@smiskol.com>
2023-01-02 12:50:25 -0800

    converter fixes

--- selfdrive/assets/videos/convert-videos.sh
+++ selfdrive/assets/videos/convert-videos.sh
@@ -6,7 +6,7 @@ echo "converting videos to mpeg4"
 mkdir -p out/
 
 for f in *.mp4; do
-  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -c:v h264 -strict -2 "out/$f"
+  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -c:v h264 -b:v 2000k -pix_fmt yuv420p -strict -2 "out/$f"
 done;
 
 echo "Success!"
