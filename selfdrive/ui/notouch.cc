a183e457c199d5d4027f5df548a4b8b8475a2e88
Shane Smiskol <shane@smiskol.com>
2023-01-02 19:57:47 -0800

    fix filter

--- selfdrive/ui/notouch.cc
+++ selfdrive/ui/notouch.cc
@@ -22,7 +22,10 @@ MainWindowNoTouch::MainWindowNoTouch(QWidget *parent) : QWidget(parent) {
   main_layout = new QVBoxLayout(this);
   main_layout->setMargin(0);
 
+  QStringList filters;
+  filters << "*.mp4" << "*.mkv";
   QDir videos_path = QDir(VIDEOS_PATH);
+  videos_path.setNameFilters(filters);
   QString content_name;
   while ((content_name = MultiOptionDialog::getSelection(tr("Select content"), videos_path.entryList(), "", this)).isEmpty()) {
     qDebug() << "No content selected!";
