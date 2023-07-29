02a5dc0fd80132dc85ed641b5fc64cd7238d50f4
Shane Smiskol <shane@smiskol.com>
2023-07-28 22:23:09 -0700

    fix download_videos.py in case of bad entries

--- selfdrive/assets/videos/download_videos.py
+++ selfdrive/assets/videos/download_videos.py
@@ -5,6 +5,7 @@ import requests
 import os
 import shutil
 from common.basedir import BASEDIR
+import traceback
 
 AZ_BASEDIR = "https://commadataci.blob.core.windows.net/cesdemo"
 MANIFEST = f"{AZ_BASEDIR}/manifest.txt"
@@ -19,12 +20,15 @@ if manifest.status_code != 200:
   sys.exit(1)
 
 manifest = manifest.content.decode('utf-8').split('\n')
-manifest = [f.replace('out/', '') for f in manifest]
+manifest = [f.replace('out/', '') for f in manifest if len(f)]
 
 for f in manifest:
   print(f'Downloading {f}')
   file_path = os.path.join(VIDEOS_PATH, f)
-  urllib.request.urlretrieve(os.path.join(AZ_BASEDIR, "out", f), file_path)
+  try:
+    urllib.request.urlretrieve(os.path.join(AZ_BASEDIR, "out", f), file_path)
+  except Exception:
+    print(traceback.format_exc())
 
 for f in os.listdir(VIDEOS_PATH):
   if f not in manifest:
