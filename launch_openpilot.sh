41b7bb526869772af585587335823883722e876b
Shane Smiskol <shane@smiskol.com>
2023-07-28 23:46:47 -0700

    Revert "update first and re-launch"
    This reverts commit ec2771bad4178927813e6a1bd22daf2b2588075b.

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,38 +1,32 @@
 #!/usr/bin/bash
 # wait for weston to come up
 sleep 5
-pkill -f spinner
 
 if [ -z "$BASEDIR" ]; then
   BASEDIR="/data/openpilot"
 fi
 export PYTHONPATH="/data/openpilot"
 
-# build
+pkill -f spinner
+# launch spinner
 echo "Building" | ./selfdrive/ui/spinner &
 spinner_pid=$!
 
+# build
 scons selfdrive/ui/_notouch selfdrive/ui/_ui -j8
 kill -9 $spinner_pid
 
 # launch ui and let user set up ssh
 ./selfdrive/ui/ui
 
-# update if not done already
-if [[ "$UPDATE_DONE" != "true" ]]; then
-  echo "Fetching updates" | ./selfdrive/ui/spinner &
-  spinner_pid=$!
+echo "Fetching updates" | ./selfdrive/ui/spinner &
+spinner_pid=$!
 
-  git fetch origin $(git rev-parse --abbrev-ref HEAD)
-  git reset --hard "@{u}"
-  git submodule update --init --recursive -f
-  kill -9 $spinner_pid
-
-  # export update status so next run doesn't fetch
-  export UPDATE_DONE="true"
-  exec /bin/bash "$0"
-  exit 0
-fi
+# update
+git fetch origin $(git rev-parse --abbrev-ref HEAD)
+git reset --hard "@{u}"
+git submodule update --init --recursive -f
+kill -9 $spinner_pid
 
 # build again after update
 echo "Building" | ./selfdrive/ui/spinner &
