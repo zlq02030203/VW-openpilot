4c5c3ed2b645b389b9ed83a703c5c2dead126c44
Shane Smiskol <shane@smiskol.com>
2023-07-28 23:04:55 -0700

    just use old working compression quality with hevc

--- selfdrive/assets/videos/convert-videos.sh
+++ selfdrive/assets/videos/convert-videos.sh
@@ -8,8 +8,8 @@ mkdir -p out/
 # Place videos in selfdrive/assets/videos and upload out/ to ces azure bucket
 
 for f in *.mp4; do
-#  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -f rawvideo -c:v h264 -b:v 4000k -pix_fmt yuv420p -strict -2 "out/$f.hevc"
-  fmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -f rawvideo -c:v h264 -crf 20 -pix_fmt yuv420p -strict -2 "out/$f.hevc"
+  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -f rawvideo -c:v h264 -b:v 4000k -pix_fmt yuv420p -strict -2 "out/$f.hevc"
+#  ffmpeg -y -i "$f" -vf scale=2160:1080 -r 20 -f rawvideo -c:v h264 -crf 23 -maxrate 4000K -bufsize 4000K -pix_fmt yuv420p -strict -2 "out/$f.hevc"
 done;
 
 echo "Success!"
