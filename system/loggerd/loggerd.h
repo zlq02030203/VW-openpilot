29935c57f31ca40c1bb9a545b30b66b6e6ed7050
Cameron Clough <cameronjclough@gmail.com>
2025-01-06 14:35:35 +0000

    increase qcam bitrate and resolution

--- system/loggerd/loggerd.h
+++ system/loggerd/loggerd.h
@@ -100,10 +100,13 @@ const EncoderInfo stream_driver_encoder_info = {
 const EncoderInfo qcam_encoder_info = {
   .publish_name = "qRoadEncodeData",
   .filename = "qcamera.ts",
-  .bitrate = QCAM_BITRATE,
+  .bitrate = 4 * QCAM_BITRATE,
   .encode_type = cereal::EncodeIndex::Type::QCAMERA_H264,
-  .frame_width = 526,
-  .frame_height = 330,
+  // AR/OX (1928x1208)
+  // OS04C10 (1344x760)
+  // QCAM (526x330)
+  .frame_width = 1052,
+  .frame_height = 660,
   INIT_ENCODE_FUNCTIONS(QRoadEncode),
 };
 
